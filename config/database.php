bb89d3464d2ab093ca985da91ee081772a9c63de
dangquocdung <dungthinhvn@gmail.com>
2017-01-03 00:04:59 +0700

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -69,14 +69,14 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
+            'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'schema' => 'public'
+
         ],
 
     ],
