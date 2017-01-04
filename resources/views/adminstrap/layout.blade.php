02183d9accc004a4222bbafc071e1f3f71dc4485
dangquocdung <dungthinhvn@gmail.com>
2017-01-04 19:10:57 +0700

    edit page admin login

--- resources/views/adminstrap/layout.blade.php
+++ resources/views/adminstrap/layout.blade.php
@@ -54,7 +54,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-10">
-            <h1><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Bảng điều khiển <small>dùng để quản trị nội dung</small></h1>
+            <h1><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> <a href="adminstrap"> Bảng điều khiển </a> <small>dùng để quản trị nội dung</small></h1>
           </div>
           <div class="col-md-2">
             <div class="dropdown create">
