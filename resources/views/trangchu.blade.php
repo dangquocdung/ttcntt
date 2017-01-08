606645ba099344803524d4f13bfaffdc992251c2
dangquocdung <dungthinhvn@gmail.com>
2017-01-08 13:01:21 +0700

    Add Delete function

--- resources/views/trangchu.blade.php
+++ resources/views/trangchu.blade.php
@@ -69,17 +69,22 @@
     <div class="row">
 
       <!--section title-->
-      <div class="col-md-4">
+      <div class="col-md-3">
         <a href="http://www.hatinh.gov.vn/"><img src="/upload/banner/congttdt.jpg"></a>
       </div>
 
       <!--section title-->
-      <div class="col-md-4">
+      <div class="col-md-3">
+        <a href="http://hatinhtrade.com.vn/"><img src="/upload/banner/santmdt.jpg"></a>
+      </div>
+
+      <!--section title-->
+      <div class="col-md-3">
         <a href="http://113.160.178.189:8686/"><img src="/upload/banner/dbnganh.jpg"></a>
       </div>
 
       <!--section title-->
-      <div class="col-md-4">
+      <div class="col-md-3">
         <a href="/bien-dao-viet-nam"><img src="/upload/banner/biendao.jpg"></a>
       </div>
     </div>
