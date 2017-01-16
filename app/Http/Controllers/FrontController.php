8680b5c9ed370e1c2aafcce23c67aab1cdf0f2dd
dangquocdung <dungthinhvn@gmail.com>
2017-01-16 09:50:51 +0700

    add diem bao

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -67,49 +67,23 @@ class FrontController extends Controller
   		$url = "http://ictnews.vn/rss/thoi-su";
   		$xml = simplexml_load_file($url);
 
+      DiemBao::getQuery()->delete();
+
 
   		foreach($xml->channel->item as $entry){
-
-  			// $title = $entry->title;
-  			// $tieudekhongdau = changeTitle($title);
-  			// $link = $entry->link;
-  			// $loaitin= substr($link, 35,strlen($link)-35);
-  			// $loaitin= substr($loaitin, 0, strpos($loaitin,'/'));
-  			// $description = $entry->description;
-  			// $pubDate = $entry->pubDate;
-  			// $hinh = $entry->children('http://search.yahoo.com/mrss/')->thumbnail->attributes();
-        //
-  			// //
-  			// $slt = LoaiTin::where('TenKhongDau','like',$loaitin)->count();
-        //
-  			// if ($slt==0){
-        //
-  			// 	$lt = new LoaiTin;
-  			// 	$lt->idTheLoai = 1;
-  			// 	$lt->TenKhongDau = $loaitin;
-  			// 	$lt->save();
-  			// }
-        //
-  			// $ltm = LoaiTin::where('TenKhongDau','like',$loaitin)->first();
-        //
-
-  			$kttin = DiemBao::where('tieudekhongdau','like',$tieudekhongdau)->count();
-
-  			if ($kttin == 0){
 
   			$tindb = new DiemBao;
 
         $tindb->loaitin_id = '1';
         $tindb->title = $entry->title;
-        $tindb->tieudekhongdau = changeTitle($entry->title);
+        $tindb->tieudekhongdau = 'tieu-de-khong-dau';
         $tindb->description = $entry->description;
         $tindb->link = $entry->link;
         $tindb->pubDate = $entry->pubDate;
 
-
   			$tindb->save();
 
-  			}
+
 
   		}
 
--- resources/views/diembao.blade.php
+++ resources/views/diembao.blade.php
@@ -32,23 +32,15 @@
                     <div class="time">{{$db->pubDate}}</div>
                     <div class="events">
                         <div class="events-header">
-                          {{$tt->title}}
-
-                          @if (Auth::user() && Auth::user()->quyen==5)
-                            <div class="sua-xoa-item">
-                              <a onclick="return confirm('Bạn muốn sửa mục này?')" href="/adminstrap/sua-tin-tuc/{{$tt->id}}"><i class="fa fa-pencil" aria-hidden="true"></i></a>
-                              <a onclick="return confirm('Bạn muốn xóa mục này?')" href="/adminstrap/xoa-tin-tuc/{{$tt->id}}"><i class="fa fa-times" aria-hidden="true"></i></a>
-                            </div>
-                          @endif
+                          {{$db->title}}
 
                         </div>
                         <div class="events-body">
                             <div class="row">
                                 <div class="events-desc">
-                                  {{$db->description}}.<a href="tin-tuc/{{$db->id}}"><em>đọc tiếp...</em></a>
+                                  {!! $db->description !!}
+                                  <a href="{{$db->link}}" target="_blank"><em>đọc tiếp</em></a>
                                 </div>
-
-
 
 
                             </div>
