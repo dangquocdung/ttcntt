98e296ff05ecb296c86ccf5ad9f89c245f9831b7
dangquocdung <dungthinhvn@gmail.com>
2017-01-16 11:47:14 +0700

    add diem bao

--- resources/views/adminstrap/dieuhanh/layout.blade.php
+++ resources/views/adminstrap/dieuhanh/layout.blade.php
@@ -235,7 +235,7 @@
     </section>
 
     <footer id="footer">
-      <p>Copyright <a href="danquocdung.com">Đặng Quốc Dũng</a>, &copy; 2017. All rights reserved.</p>
+      <p>Copyright <a href="http://danquocdung.com">Đặng Quốc Dũng</a> &copy; 2017. All rights reserved.</p>
     </footer>
 
 
