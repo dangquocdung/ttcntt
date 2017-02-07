08182bacb658b3ba69b5d862afbaca1290b091a8
dangquocdung <dungthinhvn@gmail.com>
2017-02-07 14:59:53 +0700

    dinhdau2017

--- resources/views/flayout/index.blade.php
+++ resources/views/flayout/index.blade.php
@@ -239,14 +239,15 @@
 
         var owl = $("#owl-demo");
 
-        
+
 
         owl.owlCarousel({
             items : 5, //10 items above 1000px browser width
             itemsDesktop : [1000,5], //5 items between 1000px and 901px
             itemsDesktopSmall : [900,3], // betweem 900px and 601px
             itemsTablet: [600,2], //2 items between 600 and 0
-            itemsMobile : false
+            itemsMobile : false,
+            autoPlay: true
 
              // itemsMobile disabled - inherit from itemsTablet option
         });
