928968dd2bc780f5bce7898ec50d1ca730918453
dangquocdung <dungthinhvn@gmail.com>
2017-02-07 15:02:51 +0700

    dinhdau2017

--- resources/views/trangchu.blade.php
+++ resources/views/trangchu.blade.php
@@ -65,15 +65,27 @@
           </div>
         @endforeach
 
-
-
       </div>
-
-
-
-
     </div>
   </div>
+  <script type="text/javascript">
+
+      var owl = $("#owl-sanpham");
+
+
+
+      owl.owlCarousel({
+          items : 3, //10 items above 1000px browser width
+          itemsDesktop : [1000,5], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,3], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 0
+          itemsMobile : false,
+          autoPlay: true
+
+           // itemsMobile disabled - inherit from itemsTablet option
+      });
+
+  </script>
 </div>
 
 <div class="section-a">
