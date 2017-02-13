7f07e567aab96890cc714cc7cdab143013d50ca9
dangquocdung <dungthinhvn@gmail.com>
2017-02-13 08:25:58 +0700

    add dhcv project

--- resources/views/adminstrap/layout.blade.php
+++ resources/views/adminstrap/layout.blade.php
@@ -29,7 +29,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Welcome, {{ Auth::user()->name }}</a>
+          <a class="navbar-brand" href="/">&barr Trở về</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
 
@@ -46,18 +46,26 @@
 
           <ul class="nav navbar-nav navbar-right">
             <!-- <li><a href="#">Welcome, {{ Auth::user()->name }}</a></li> -->
-            <li><a href="/">Trang chủ</a></li>
-            <li>
-              <a href="{{ url('/logout') }}"
-                  onclick="event.preventDefault();
-                           document.getElementById('logout-form').submit();">
-                  Logout
-              </a>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
 
-              <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                  {{ csrf_field() }}
-              </form>
+                <ul class="dropdown-menu" role="menu">
+                  <li>
+                    <a href="{{ url('/logout') }}"
+                        onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                  </li>
+                </ul>
             </li>
+
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -76,7 +84,7 @@
 
             @yield('content')
 
-          
+
         </div>
       </div>
     </section>
